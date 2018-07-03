e7cc534cd06c4731eb646112a2f201887fcdd912
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-03 17:43:15 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,9 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
   
   def full_name
-    self.first_name
+    first = self.first_name
+    last = self.last_name
+    
   end 
   def list_roles
     self.characters.each {|char| char.name}
